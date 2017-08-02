dab9673552b4b8892b7705882e9fce2f612c9372
MingmingXiong <babybearming@163.com>
2017-08-02 16:24:22 +0800

    modify

--- myfunction.py
+++ myfunction.py
@@ -5,6 +5,9 @@ Created on Wed Aug  2 09:58:06 2017
 
 @author: bbr
 """
+import urllib2
+import sys
+from xml.etree import ElementTree
 
 def cimissread(stationid,element,timeRange,hourorDay=0):
 """
@@ -14,12 +17,17 @@ inputvar: one stationid, one meterological element, timeRange and 1 for 'hour'
 output: array of cimiss data
 'day' : year month day 'value'
 'hour': year month day time 'value'
+"""
 
-"""    
+# initial the element
     if hourorDay==0:
         dataSetName='SURF_CHN_MUL_DAY'
+        var1=['Station_Id_d','Year','Mon','Day']
     else:
         dataSetName='SURF_CHN_MUL_HOR'
+        var1=['Station_Id_d','Year','Mon','Day','Time']
+    var=var1+element
+    elements=','.join(var)
         
     baseUrl = 'http://10.226.89.17:8008/cimiss-web/api?userId=user_ssny&pwd=23333559\
 &interfaceId=getSurfEleByTimeRangeAndStaID\
@@ -27,4 +35,24 @@ output: array of cimiss data
 &elements=%s\
 &timeRange=%s\
 &staIds=%s\
-&dataFormat=' % (dataSetName,element,timeRange,stationId)
+&dataFormat=' % (dataSetName,elements,timeRange,stationId)
+
+    dateFormat='xml'
+	req = urllib2.Request(baseUrl + dataFormat)
+	response = urllib2.urlopen(req)
+	data = response.read()
+	# print data
+	root = ElementTree.fromstring(data)
+	lst_node = root.getiterator("DS")
+
+	for node in lst_node[0]:
+		Station_Id_d.append(node.get('Station_Id_d'))
+		Year.append(node.get('Year'))
+		Mon.append(node.get('Mon'))
+		Day.append(node.get('Day'))
+		meElment.append(node.get(var_query[0]))
+
+if __name__=="__main__":
+    cimissread('54517','TEM_Avg','[20160101000000,20161231000000]')
+
+
